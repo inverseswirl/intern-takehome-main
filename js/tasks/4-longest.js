9511711956916e1787742c650c0bd48bae05eb5a
inverseswirl <68209878+inverseswirl@users.noreply.github.com>
2022-03-06 23:04:54 +1100

    task-4/longest

--- js/tasks/4-longest.js
+++ js/tasks/4-longest.js
@@ -6,5 +6,24 @@
  * @returns {String} The longest string from within the input array.
  */
 export default function longestString(strings) {
-    
+
+    let longest_string = ''
+    let length_object = {}
+    strings.map((word)=>{
+        if(typeof word === 'string'){
+          length_object[word] = word.length   //object with words and their lengths as values.
+        }
+    })
+
+   const values_array = Object.values(length_object) //get values
+   const max_length = Math.max(...values_array) //get max length
+   
+  for (const word in length_object){
+    if(length_object[word] === max_length){ //match word with max_length
+      longest_string = word   
+      return longest_string
+    }
+   }
+
 }
+    
