0a149d99ccabf112f05f74b7befeb3b9e83d8f57
inverseswirl <68209878+inverseswirl@users.noreply.github.com>
2022-03-06 22:42:02 +1100

    task-5/array-sum

--- js/tasks/5-array-sum.js
+++ js/tasks/5-array-sum.js
@@ -6,5 +6,17 @@
  * @returns {Number} The sum of all integers contained in the input, at any level.
  */
 export default function arraySum(startOfTree) {
+  let sum = 0
+  let flat_array = startOfTree.flat(Infinity) //flattens deep nested arrays to single level
+    
+  flat_array.map((element)=>{
+    if(typeof element === 'number'){  //ignores any other data type other than number
+        sum += element  //adds all integers
+    }
+   })
+   
+   return sum;
 
 }
+
+
