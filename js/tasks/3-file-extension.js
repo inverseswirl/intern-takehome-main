26f947eab06fa058d2842995df340cae86d4839d
inverseswirl <68209878+inverseswirl@users.noreply.github.com>
2022-03-06 22:23:43 +1100

    task-3/file-extension

--- js/tasks/3-file-extension.js
+++ js/tasks/3-file-extension.js
@@ -6,5 +6,13 @@
  * @returns {String} The file extension (with no period), or false if there is none.
  */
 export default function getFileExtension(filename) {
-
+    if(filename.includes('.')){ //if extension is present
+        let extension_index = filename.indexOf('.')
+        let substring = filename.substring(extension_index + 1)
+        return substring
+    }else{   //no extension is present
+        return false;
+    }
 }
+
+
